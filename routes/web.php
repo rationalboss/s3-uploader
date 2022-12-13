cd14fb137865d696ae95ab739a524fea14f616a5
rationalboss <rationalboss@gmail.com>
2022-12-13 18:29:43 +0800

    change method to POST

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/gh', function () {
+Route::post('/gh', function () {
     shell_exec('cd /home/ubuntu/htdocs; git remote update; git checkout -f; git pull origin main; composer install; php artisan cache:clear; php artisan migrate --force;');
     return 'OK';
 });
